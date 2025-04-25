f170a74b9a3d62b3e99219cb4d40de0a979c289d
Izalhadji31 <ryzalhadji@gmail.com>
2025-04-25 14:05:36 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Collection ;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+
+            ->orderBy('is_done','asc')
+
+            ->orderBy('created_at','desc')
+
+            ->get();
+     
+        //dd($todos);
+        return view('todo.index', compact('todos'));
     }
 
     public function create()
@@ -20,5 +31,42 @@ class TodoController extends Controller
     {
         return view('todo.edit');
     }
-    
+
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+
+            'title' => 'required|max:255',
+
+        ]);
+
+        //Practical
+
+        //$todo = new Todo;
+
+        //$todo->title = $request->title;
+
+        //$todo->user_id = auth()->user()->id();
+
+        //$todo->save();
+
+
+
+        // Query Builder way
+        // DB::table('todos')->insert([
+        //      'title' => $request->title,
+        //      'user_id' => auth()->user()->id,
+        //      'created_at' => now(),
+        //      'updated_at' => now(),
+        // ]);
+
+        // Eloquent Way - Readable
+
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
+    }
 }
