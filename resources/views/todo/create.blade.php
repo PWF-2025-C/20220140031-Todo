cdba7ac4c2a8768bf5cf339c96adf2f18a564e5b
Izalhadji31 <ryzalhadji@gmail.com>
2025-04-25 18:31:43 +0700

    create, read, search data

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -1,17 +1,44 @@
-<x-app-layout name="header">
-    <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-        {{ __('Todo') }}
-    </h2>
-</x-slot>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('Todo') }}
+        </h2>
+    </x-slot>
 
-<div class="py-12">
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
-            <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
-                {{ __('Create Todo Page') }}
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                <form method="post" action="{{ route('todo.store') }}" class="">
+                    @csrf
+                    @method('post')
+
+                    <div class="mb-6">
+                        <x-input-label for="title" :value="('Title')" />
+                        <x-text-input 
+                            id="title" 
+                            name="title" 
+                            type="text" 
+                            class="block w-full mt-1" 
+                            required 
+                            autofocus 
+                            autocomplete="title" 
+                        />
+                        <x-input-error class="mt-2" :messages="$errors->get('title')" />
+                    </div>
+
+                    <div class="flex items-center gap-4">
+                        <x-primary-button>
+                            {{ __('Save') }}
+                        </x-primary-button>
+
+                        <x-link-button href="{{ route('todo.index') }}">
+                            {{ __('Cancel') }}
+                        </x-link-button>
+                    </div>
+                </form>
             </div>
         </div>
+        </div>
     </div>
-</div>
-
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
-    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    Route::get('/todo',[TodoController::class,'index'])->name('todo.index');
+    Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
+    Route::get('/todo/create',[TodoController::class,'create'])->name('todo.create');
+    Route::get('/todo/edit',[TodoController::class,'edit'])->name('todo.edit');
 
-    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::get('/user',[UserController::class,'index'])->name('user.index');
+    Route::get('/user/{id}', [UserController::class, 'show'])->name('users.show'); // Opsional tambahan
 });
 
 require __DIR__.'/auth.php';
